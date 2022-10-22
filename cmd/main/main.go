9f063d65e4d4593f53ed9bde8ea6a6eb45d188c8
Stefan Tumey <stef@Stefans-Air.lan>
2022-10-22 15:38:20 -0400

    Set up main.go

---
+++ cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/stumey/go-BookManagementSystem/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
